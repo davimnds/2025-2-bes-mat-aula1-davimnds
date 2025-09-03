6f3e5f6fbbafd8c737bc0511f6e8f8ba05c8e125
davimnds <lgdntroca777@gmail.com>
2025-09-03 16:48:32 -0300

    Update User.java

--- src/main/java/cms/user/User.java
+++ src/main/java/cms/user/User.java
@@ -1,5 +1,38 @@
 package cms.user;
 
-public class User {
+// A classe 'User' agora é abstrata.
+public abstract class User {
+    private final int id;
+    private final String nome;
+    private final String email;
+    private final String password;
 
+    // Construtor que as classes filhas chamarão.
+    public User(int id, String nome, String email, String password) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.password = password;
+    }
+
+    // Métodos que todos os usuários compartilham.
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public boolean checkPassword(String p) {
+        return password.equals(p);
+    }
+
+    // Método abstrato que cada tipo de usuário implementará de forma diferente.
+    // Isso garante o polimorfismo.
+    public abstract void doAction();
 }
